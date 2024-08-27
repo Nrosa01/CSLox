5dd970e2754c268811b51891a0790db534f18e9f
Rioni <nrosa01@ucm.es>
2024-08-27 16:06:16 +0200

    Init project

---
+++ Src/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLox.Src
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"There are {args.Length} arguments");
+
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: jlox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void Run(in string code)
+        {
+            foreach (string word in code.WordList())
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void RunFile(in String filename)
+        {
+            Console.WriteLine($"Running file {filename}");
+            Run(File.ReadAllText(filename));
+        }
+
+        static void RunPrompt()
+        {
+            string line;
+
+            while (true)
+            {
+                Console.Write("> ");
+                line = Console.ReadLine();
+                if (line.Length == 0)
+                    break;
+                else
+                    Run(line);
+            }
+        }
+    }
+}
---
+++ Src/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLox.Src
+{
+    static public class StringExtensions
+    {
+        /// <summary>
+        /// Sweep over text
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> WordList(this string Text)
+        {
+            int cIndex = 0;
+            int nIndex;
+            while ((nIndex = Text.IndexOf(' ', cIndex + 1)) != -1)
+            {
+                int sIndex = (cIndex == 0 ? 0 : cIndex + 1);
+                yield return Text.Substring(sIndex, nIndex - sIndex);
+                cIndex = nIndex;
+            }
+            yield return Text.Substring(cIndex + 1);
+        }
+    }
+}
