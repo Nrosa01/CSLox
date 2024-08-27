77af50c70cc185922392b2a54addfe117bb98e86
Rioni <nrosa01@ucm.es>
2024-08-27 18:32:57 +0200

    Lexer done, migrated to .NET 8

--- Src/Lox/Lox.cs
+++ Src/Lox/Lox.cs
@@ -1,63 +1,76 @@
-﻿using System;
+﻿using CSLox.Src.Utils;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSLox.Src
+namespace CSLox.Src.Lox
 {
-    internal class Program
+    internal class Lox
     {
+        static bool hadError = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine($"There are {args.Length} arguments");
-
             if (args.Length > 1)
             {
                 Console.WriteLine("Usage: jlox [script]");
                 Environment.Exit(64);
             }
             else if (args.Length == 1)
-            {
                 RunFile(args[0]);
-            }
             else
-            {
                 RunPrompt();
-            }
-
 
             Console.ReadLine();
         }
 
         static void Run(in string code)
         {
-            foreach (string word in code.WordList())
-            {
-                Console.WriteLine(word);
-            }
+            Scanner scanner = new Scanner(code);
+            List<Token> tokens = scanner.ScanTokens();
+
+            foreach (Token token in tokens)
+                Console.WriteLine(token);
         }
 
         static void RunFile(in String filename)
         {
             Console.WriteLine($"Running file {filename}");
             Run(File.ReadAllText(filename));
+            if (hadError) Environment.Exit(65);
         }
 
         static void RunPrompt()
         {
-            string line;
+            string? line;
 
             while (true)
             {
                 Console.Write("> ");
                 line = Console.ReadLine();
-                if (line.Length == 0)
+                if (line == null)
                     break;
                 else
+                {
+                    hadError = false;
                     Run(line);
+                }
             }
+        }
+
+        public static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where,
+                                   string message)
+        {
+            Console.Error.WriteLine($"[line {line}] Error {where}: {message}");
+            hadError = true;
         }
     }
 }
---
+++ Src/Lox/Scanner.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLox.Src.Lox
+{
+    using static CSLox.Src.Lox.TokenType;
+
+    internal class Scanner
+    {
+        readonly string source;
+        readonly List<Token> tokens;
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+        private static readonly Dictionary<string, TokenType> keywords = new Dictionary<string, TokenType>()
+        {
+            { "and", TokenType.AND },
+            { "class", TokenType.CLASS },
+            { "else", TokenType.ELSE },
+            { "false", TokenType.FALSE },
+            { "for", TokenType.FOR },
+            { "fun", TokenType.FUN },
+            { "if", TokenType.IF },
+            { "nil", TokenType.NIL },
+            { "or", TokenType.OR },
+            { "print", TokenType.PRINT },
+            { "return", TokenType.RETURN },
+            { "super", TokenType.SUPER },
+            { "this", TokenType.THIS },
+            { "true", TokenType.TRUE },
+            { "var", TokenType.VAR },
+            { "while", TokenType.WHILE }
+        };
+
+        public Scanner(string source)
+        {
+            this.source = source;
+            tokens = new List<Token>();
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                start = current;
+                ScanToken();
+            }
+
+            tokens.Add(new Token(EOF, "", null, line));
+            return tokens;
+        }
+
+        void ScanToken()
+        {
+            char c = Advance();
+            switch (c)
+            {
+                case '(': AddToken(LEFT_PAREN); break;
+                case ')': AddToken(RIGHT_PAREN); break;
+                case '{': AddToken(LEFT_BRACE); break;
+                case '}': AddToken(RIGHT_BRACE); break;
+                case ',': AddToken(COMMA); break;
+                case '.': AddToken(DOT); break;
+                case '-': AddToken(MINUS); break;
+                case '+': AddToken(PLUS); break;
+                case ';': AddToken(SEMICOLON); break;
+                case '*': AddToken(STAR); break;
+                case '!':
+                    AddToken(Match('=') ? BANG_EQUAL : BANG);
+                    break;
+                case '=':
+                    AddToken(Match('=') ? EQUAL_EQUAL : EQUAL);
+                    break;
+                case '<':
+                    AddToken(Match('=') ? LESS_EQUAL : LESS);
+                    break;
+                case '>':
+                    AddToken(Match('=') ? GREATER_EQUAL : GREATER);
+                    break;
+                case '/':
+                    if (Match('/'))
+                    {
+                        // A comment goes until the end of the line.
+                        while (Peek() != '\n' && !IsAtEnd()) Advance();
+                    }
+                    else
+                        AddToken(SLASH);
+                    break;
+                case '"': String(); break;
+                case ' ':
+                case '\r':
+                case '\t':
+                    // Ignore whitespace.
+                    break;
+
+                case '\n':
+                    line++;
+                    break;
+                default:
+                    if (Char.IsDigit(c))
+                        Number();
+                    else if (IsAlpha(c))
+                        Identifier();
+                    else Lox.Error(line, "Unexpected character.");
+                    break;
+            }
+        }
+
+        private void Identifier()
+        {
+            while (IsAlphaNumeric(Peek())) Advance();
+            string text = source[start..current];
+            
+            // Try to get the type from the keywords dictionary
+            if (!keywords.TryGetValue(text, out TokenType type))
+                // If the keyword doesn't exist in the dictionary, it's an identifier
+                type = IDENTIFIER;
+
+            AddToken(type);
+        }
+
+        private bool IsAlpha(char c)
+        {
+            return (c >= 'a' && c <= 'z') ||
+                   (c >= 'A' && c <= 'Z') ||
+                    c == '_';
+        }
+
+        private bool IsDigit(char c) => c >= '0' && c <= '9';
+
+        private bool IsAlphaNumeric(char c) => IsAlpha(c) || IsDigit(c);
+
+        private void String()
+        {
+            while (Peek() != '"' && !IsAtEnd())
+            {
+                if (Peek() == '\n') line++;
+                Advance();
+            }
+
+            if (IsAtEnd())
+            {
+                Lox.Error(line, "Unterminated string.");
+                return;
+            }
+
+            // The closing "
+            Advance();
+
+            // Trim the surrounding quotes
+            string value = source[(start + 1)..(current - 1)];
+            AddToken(STRING, value);
+        }
+
+        private char Peek()
+        {
+            if (IsAtEnd()) return '\0';
+            return source[current];
+        }
+
+        private char PeekNext()
+        {
+            if (current + 1 >= source.Length) return '\0';
+            return source[current + 1];
+        }
+
+        private bool Match(char expected)
+        {
+            if (IsAtEnd()) return false;
+            if (source[current] != expected) return false;
+
+            current++;
+            return true;
+        }
+
+        private void Number()
+        {
+            while (Char.IsDigit(Peek())) Advance();
+
+            // Look for a fractional part.
+            if (Peek() == '.' && Char.IsDigit(PeekNext()))
+            {
+                // Consume the "."
+                Advance();
+
+                while (Char.IsDigit(Peek())) Advance();
+            }
+
+            AddToken(NUMBER, Double.Parse(source[start..current]));
+        }
+
+        private char Advance()
+        {
+            return source[current++];
+        }
+
+        private void AddToken(TokenType type)
+        {
+            AddToken(type, null);
+        }
+
+        private void AddToken(TokenType type, object? literal)
+        {
+            string text = source[start..current];
+            tokens.Add(new Token(type, text, literal, line));
+        }
+
+        bool IsAtEnd() => current >= source.Length;
+    }
+}
